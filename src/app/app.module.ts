1eda39cb5ce5c6303fc1199aab8bc2ac238ba289
Anthony Pena <anthony.pena@outlook.fr>
2020-07-01 12:40:25 +0200

    feature - setup pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { UnitCardComponent } from './unit-card/unit-card.component';
 
@@ -17,6 +19,7 @@ import { UnitCardComponent } from './unit-card/unit-card.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
 
     MatCardModule,
     MatInputModule,
